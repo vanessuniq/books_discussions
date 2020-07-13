3f2f01f2c56233c80f8cd800dfcb7d3bb9d0e782
vanessuniq <vanessuniq@gmail.com>
2020-07-12 20:36:49 -0400

    added users controller and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'signup', to: "users#new"
   root 'static_pages#home'
   get '/home', to: "static_pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
