6535428f1e02c41055a419611e9c4484ec96530c
vanessuniq <vanessuniq@gmail.com>
2020-07-16 16:02:32 -0400

    finish user signup and test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome, You have successfully created your account."
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def show
@@ -8,4 +19,11 @@ class UsersController < ApplicationController
 
     #debugger
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
