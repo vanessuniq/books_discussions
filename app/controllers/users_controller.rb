099afa688cbc4c99312f62c92d87a7d9a028ff58
vanessuniq <vanessuniq@gmail.com>
2020-07-16 01:58:04 -0400

    users routes and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    # add logic for authorization
+
+    #debugger
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'signup', to: "users#new"
   root 'static_pages#home'
   get '/home', to: "static_pages#home"
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
