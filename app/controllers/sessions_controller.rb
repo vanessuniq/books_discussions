e8b3d78301956b012ad4261695fcbf9b52d54b4e
vanessuniq <vanessuniq@gmail.com>
2020-07-17 02:21:38 -0400

    sessions create action and test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:id] = user.id
+      flash[:success] = "Welcome back #{user.username}!"
+      redirect_to user
+    else
+      flash.now[:danger] = "Invalid email/password combination. Please try again or sign up below if not a member."
+      render :new
+    end
   end
 
   def destroy
     
   end
-  
+
 end
