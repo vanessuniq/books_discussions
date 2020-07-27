68ab041feba1b77497fa1dde630a125946cd2166
vanessuniq <vanessuniq@gmail.com>
2020-07-26 20:57:04 -0400

    add google tokens to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_063328) do
+ActiveRecord::Schema.define(version: 2020_07_27_004548) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
